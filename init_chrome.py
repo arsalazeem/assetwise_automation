366b976e88ca47eda4c6114ca0a68407207ba93d
arsalazeem <azeemarsal@gmail.com>
2021-02-12 16:45:15 +0500

    fixed chrome path and version

--- init_chrome.py
+++ init_chrome.py
@@ -1,18 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
 def start():
-    driver = webdriver.Chrome(executable_path='chromedriver_win32//chromedriver.exe')
+    driver = webdriver.Chrome(executable_path='drivers/chromedriver.exe')
     driver.maximize_window()
     return driver
 
 def start_minize():
-    driver = webdriver.Chrome(executable_path='chromedriver_win32//chromedriver.exe')
+    driver = webdriver.Chrome(executable_path='drivers/chromedriver.exe')
     driver.minimize_window()
     return driver
+
+
+start()
